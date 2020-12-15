b0485c724e334092848b6eac8b8fb1990b57a9e1
Kanad Shrikar <kanadpardeshi@cse.iitb.ac.in>
2020-12-15 13:59:31 +0530

    Bonus task done

--- app/src/main/java/com/example/outlab9/GetNews.java
+++ app/src/main/java/com/example/outlab9/GetNews.java
@@ -11,12 +11,19 @@ import java.util.ArrayList;
 import java.util.concurrent.Callable;
 
 public class GetNews implements Callable<ArrayList<String>>{
+
+    private ArrayList<String> newsArray;
+
+    public GetNews(ArrayList<String> arr) {
+        newsArray = arr;
+    }
+
     public ArrayList<String> call() {
         String news;
-        ArrayList<String> values = new ArrayList<String>();
         OkHttpClient client = new OkHttpClient();
+        int offset = newsArray.size();
         Request request = new Request.Builder()
-                .url("https://bing-news-search1.p.rapidapi.com/news?safeSearch=Off&textFormat=Raw")
+                .url("https://bing-news-search1.p.rapidapi.com/news?count=15&offset=" + offset + "&mkt=en-IN&safeSearch=Off&category=World&textFormat=Raw")
                 .get()
                 .addHeader("x-bingapis-sdk", "true")
                 .addHeader("x-rapidapi-key", "3f3723aa27mshb763be6f66a7c88p1b4bedjsn1a77cf33295a")
@@ -26,15 +33,14 @@ public class GetNews implements Callable<ArrayList<String>>{
             Response response = client.newCall(request).execute();
             news = response.body().string();
             JSONObject reader = new JSONObject(news);
-            ArrayList<String> headlineList = new ArrayList<String>();
             JSONArray newsObjects = reader.getJSONArray("value");
             for (int i = 0; i<newsObjects.length(); i++) {
                 JSONObject v = newsObjects.getJSONObject(i);
-                values.add(v.getString("name"));
+                newsArray.add(v.getString("name"));
             }
         } catch (Exception e) {
-            values.add(e.toString());
+            newsArray.add(e.toString());
         }
-        return values;
+        return newsArray;
     }
 }
--- app/src/main/java/com/example/outlab9/MainActivity.java
+++ app/src/main/java/com/example/outlab9/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.outlab9;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -18,6 +21,10 @@ public class MainActivity extends AppCompatActivity {
     private static RecyclerView.Adapter adapter;
     private RecyclerView.LayoutManager layoutManager;
     private static RecyclerView recyclerView;
+    private ArrayList<String> news = new ArrayList<String>();
+    private boolean isLoading = false;
+    private boolean firstLoad = true;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,16 +36,85 @@ public class MainActivity extends AppCompatActivity {
         layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setItemAnimator(new DefaultItemAnimator());
+        loadNews();
+        adapter = new CustomAdapter(news);
+        recyclerView.setAdapter(adapter);
+        initScrollListener();
+    }
 
-        ArrayList<String> news = new ArrayList<String>();
-        try {
-            GetNews gn = new GetNews();
-            ExecutorService es = Executors.newSingleThreadExecutor();
-            Future<ArrayList<String>> nw = es.submit(gn);
-            news = nw.get();
-            adapter = new CustomAdapter(news);
-            recyclerView.setAdapter(adapter);
-        } catch (Exception e) {
-        }
+    private void initScrollListener() {
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+            }
+
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                LinearLayoutManager linearLayoutManager = (LinearLayoutManager) recyclerView.getLayoutManager();
+                Log.d("Loader", "Loading is required!");
+                if (!isLoading) {
+                    if (linearLayoutManager != null && linearLayoutManager.findLastCompletelyVisibleItemPosition() == news.size()-1) {
+                        Log.d("Loader", "Reached end, loading news");
+                        loadNews();
+                        Log.d("Loader", "Loaded news");
+                        isLoading = true;
+                    }
+                }
+            }
+        });
+    }
+
+    private void loadNews() {
+//        try {
+//            GetNews gn = new GetNews(news);
+//            ExecutorService es = Executors.newSingleThreadExecutor();
+//            Future<ArrayList<String>>  nw = es.submit(gn);
+//            news = nw.get();
+//            if (firstLoad) {
+//                firstLoad = false;
+//            }
+//            else {
+//                recyclerView.post(new Runnable() {
+//                    public void run() {
+//                        adapter.notifyDataSetChanged();
+//                    }
+//                });
+//            }
+//            isLoading = false;
+//        } catch (Exception e) {
+//            news.add("Early error");
+//            if (firstLoad) {
+//                firstLoad = false;
+//            }
+//            else {
+//                recyclerView.post(new Runnable() {
+//                    public void run() {
+//                        adapter.notifyDataSetChanged();
+//                    }
+//                });
+//            }
+//            isLoading = false;
+//        }
+        Handler handler = new Handler();
+        handler.post(new Runnable() {
+            public void run() {
+                GetNews gn = new GetNews(news);
+                ExecutorService es = Executors.newSingleThreadExecutor();
+                Future<ArrayList<String>> nw = es.submit(gn);
+                try {
+                    news = nw.get();
+                    if (firstLoad) {
+                        firstLoad = false;
+                    } else {
+                        adapter.notifyDataSetChanged();
+                    }
+                    isLoading = false;
+                } catch (Exception e){
+                    Log.d("Error", "error");
+                }
+            }
+        });
     }
 }
